adf703fcd110632db086b8cec58bcf066d55507a
ABlackPikatchu <J.Wimbrough@gmail.com>
2022-01-22 18:30:11 -0500

    Added some items

--- src/main/java/net/cthulhuthemad/blessedemeralds/item/ModItems.java
+++ src/main/java/net/cthulhuthemad/blessedemeralds/item/ModItems.java
@@ -1,16 +1,26 @@
 package net.cthulhuthemad.blessedemeralds.item;
 
 import net.cthulhuthemad.blessedemeralds.BlessedEmeralds;
+import net.cthulhuthemad.blessedemeralds.item.custom.BlessedAppleItem;
+import net.cthulhuthemad.blessedemeralds.item.custom.ModFoodComponents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item EMERALD_SHARD = registerItem("emerald_shard",
             new Item(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)));
+    public static final Item POWERED_EMERALD_SHARD = registerItem("powered_emerald_shard",
+            new Item(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS).rarity(Rarity.RARE)));
+    public static final Item BLESSED_EMERALD_PIECE = registerItem("blessed_emerald_piece",
+            new Item(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS).rarity(Rarity.EPIC)));
+
+    public static final Item BLESSED_APPLE = registerItem("blessed_apple",
+            new BlessedAppleItem(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)
+                    .rarity(Rarity.EPIC).food(ModFoodComponents.BLESSED_APPLE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(BlessedEmeralds.MOD_ID, name), item);
