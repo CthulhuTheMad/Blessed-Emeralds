0150bc4f9455d6ca0294fe39c154cb1f181ba45e
ABlackPikatchu <J.Wimbrough@gmail.com>
2022-01-22 20:16:00 -0500

    Fixed the mcmeta files for textures. Fixed the Effects given by Blessed Apple. Added Emerald Tools(Needs Textures)

---
+++ src/main/java/net/cthulhuthemad/blessedemeralds/item/ModFoodComponents.java
@@ -0,0 +1,17 @@
+package net.cthulhuthemad.blessedemeralds.item;
+
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+    public static FoodComponent BLESSED_APPLE = new FoodComponent.Builder().hunger(4).saturationModifier(1.2F)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 6000, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 6000, 4), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 6000, 4), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 6000, 4), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 6000, 3), 1.0f)
+            .alwaysEdible()
+            .build();
+}
--- src/main/java/net/cthulhuthemad/blessedemeralds/item/ModItems.java
+++ src/main/java/net/cthulhuthemad/blessedemeralds/item/ModItems.java
@@ -2,9 +2,13 @@ package net.cthulhuthemad.blessedemeralds.item;
 
 import net.cthulhuthemad.blessedemeralds.BlessedEmeralds;
 import net.cthulhuthemad.blessedemeralds.item.custom.BlessedAppleItem;
-import net.cthulhuthemad.blessedemeralds.item.custom.ModFoodComponents;
+import net.cthulhuthemad.blessedemeralds.item.custom.ModAxeItem;
+import net.cthulhuthemad.blessedemeralds.item.custom.ModHoeItem;
+import net.cthulhuthemad.blessedemeralds.item.custom.ModPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -19,6 +23,22 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS).rarity(Rarity.RARE)));
     public static final Item BLESSED_EMERALD = registerItem("blessed_emerald",
             new Item(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS).rarity(Rarity.EPIC)));
+
+    public static final Item EMERALD_PICKAXE = registerItem("emerald_pickaxe",
+            new ModPickaxeItem(ModToolMaterial.EMERALD, 1, -2.8F,
+                    new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)));
+    public static final Item EMERALD_AXE = registerItem("emerald_axe",
+            new ModAxeItem(ModToolMaterial.EMERALD, 5.5F, -3.0F,
+                    new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)));
+    public static final Item EMERALD_HOE = registerItem("emerald_hoe",
+            new ModHoeItem(ModToolMaterial.EMERALD, -3, 0.0F,
+                    new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)));
+    public static final Item EMERALD_SHOVEL = registerItem("emerald_shovel",
+            new ShovelItem(ModToolMaterial.EMERALD, 2, -3.0F,
+                    new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)));
+    public static final Item EMERALD_SWORD = registerItem("emerald_sword",
+            new SwordItem(ModToolMaterial.EMERALD, 4, -2.4F,
+                    new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)));
 
     public static final Item BLESSED_APPLE = registerItem("blessed_apple",
             new BlessedAppleItem(new FabricItemSettings().group(ModItemGroup.BLESSED_EMERALDS)
